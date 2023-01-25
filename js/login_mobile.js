2b3b3d9fa1931f92fae8844773d2910f61592688
saleh <saleh.git.22@gamil.com>
2023-01-25 22:49:22 +0330

    add limitation for login and register  ( js )

--- js/login_mobile.js
+++ js/login_mobile.js
@@ -22,3 +22,51 @@ login_register.forEach(
         })
     }
 );
+// ! check inputs 
+
+// _1_  check password
+let submit_register = document.getElementById("submit-register");
+let submit_login = document.getElementById("submit-login");
+
+$(submit_register).click(check_register);
+$(submit_login).click(check_login);
+
+function check_register(e) {
+    let name = $("#name-register")[0];
+    let email = $("#email-register")[0];
+    let pass = $("#pass-register")[0];
+    let re_pass = $("#re-pass-register")[0];
+
+    //name
+    if (name.value.length < 3) {
+        console.log("name is so less");
+    }
+    //email
+    let length_tex = email.value.length - 10;
+    let Gmail = email.value.substring(length_tex, email.value.length);
+    if (Gmail == "@gmail.com" || Gmail == "@yahoo.com" && email.value.length > 13) {
+        console.log("email ok");
+    } else {
+        console.log('email not true');
+    }
+    // maching pass
+    if (re_pass.value == pass.value) {
+        console.log("pass ok");
+    } else {
+        console.log("pass not maching");
+    }
+
+}
+
+function check_login(e) {
+    let email = $("#email-login")[0];
+
+    //email
+    let length_tex = email.value.length - 10;
+    let Gmail = email.value.substring(length_tex, email.value.length);
+    if (Gmail == "@gmail.com" || Gmail == "@yahoo.com" && email.value.length > 13) {
+        console.log("email ok");
+    } else {
+        console.log('email not true');
+    }
+}
