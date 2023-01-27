cc36954df0ebd12269507a57a2bca2535593ecf1
Zahra <you@example.com>
2023-01-27 18:37:44 +0330

    selectors added

---
+++ js/login.js
@@ -0,0 +1,9 @@
+// ? selectors
+let switchCtn = document.querySelector("#switch-cnt");
+let switchC1 = document.querySelector("#switch-c1");
+let switchC2 = document.querySelector("#switch-c2");
+let switchCircle = document.querySelectorAll(".switch__circle");
+let switchBtn = document.querySelectorAll(".switch-btn");
+let aContainer = document.querySelector("#a-container");
+let bContainer = document.querySelector("#b-container");
+let allButtons = document.querySelectorAll(".submit");
