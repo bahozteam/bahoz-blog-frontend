3d85635170ce0c65e415ccc0491e4db30258c65c
Zahra <you@example.com>
2023-02-17 17:05:32 +0330

    second switch container place changed

--- js/login.js
+++ js/login.js
@@ -6,6 +6,7 @@ let switchCircle = document.querySelectorAll(".switch__circle");
 let switchBtn = document.querySelectorAll(".switch-btn");
 let aContainer = document.querySelector("#a-container");
 let bContainer = document.querySelector("#b-container");
+let loginContainer = document.querySelector(".login-container");
 let allButtons = document.querySelectorAll(".submit");
 
 // ! functions
@@ -21,13 +22,13 @@ let changeForm = (e) => {
 
     switchCtn.classList.toggle("is-txr");
     switchCircle[0].classList.toggle("is-txr");
-    switchCircle[1].classList.toggle("is-txr");
+    // switchCircle[1].classList.toggle("is-txr");
 
     switchC1.classList.toggle("is-hidden");
     switchC2.classList.toggle("is-hidden");
     aContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-z200");
+    // bContainer.classList.toggle("is-txl");
+    // bContainer.classList.toggle("is-z200");
 }
 
 let mainF = (e) => {
